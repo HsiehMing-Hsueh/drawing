760e5e4470bbb7d5edf7fbc6bb82580455a2425a
謝明學 <gjerry40105@gmail.com>
2023-03-31 15:23:22 +0800

    修改第2次

---
+++ README.md
@@ -0,0 +1,6 @@
+# Drawing Taiwan AQI MAP
+
+-index.py是主程式
+-tools.py是主程式的工具
+
+-test1.py是測試檔案
--- tools.py
+++ tools.py
@@ -17,31 +17,22 @@ class Site(object):
         return f"站點:{self.site_name},城市:{self.county},aqi:{self.aqi}"
 
 
-class Taiwan_AQI():
-
-    @classmethod
-    def download_aqi(cls) -> list:
-        response = requests.get(
-            f"https://data.epa.gov.tw/api/v2/aqx_p_432?api_key={password.API_KEY}&limit=1000&sort=ImportDate desc&format=CSV")
+class Taiwan_AQI(list):
+    def __init__(self):
+        super().__init__()
+        response = requests.get(f"https://data.epa.gov.tw/api/v2/aqx_p_432?api_key={password.API_KEY}&limit=1000&sort=ImportDate desc&format=CSV")
 
         if response.ok:
-            # print(response.text)
-            # file = open("./lesson17/aqi.csv",mode="w",encoding="utf -8")
-            # file.write(response.text)
-            # file.close()
             file = StringIO(response.text, newline="")
             csvReader = csv.reader(file)
             next(csvReader)
-            dataList = []
             for item in csvReader:
                 site = Site(item[0], item[1], item[2])
-                dataList.append(site)
-            return dataList
-
-            '''
-            for item in csvReader:
-                print(item[0])
-            '''
-
+                self.append(site)
+            
         else:
             raise Exception("下載失敗")
+        
+    def get_bad_3(self):
+        return self[:3]
+
